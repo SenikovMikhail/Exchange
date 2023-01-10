a0a93a3203f26390624347c52b11f1c83c60c155
SenikovMikhail <112110914+SenikovMikhail@users.noreply.github.com>
2023-01-10 14:02:36 +0300

    Add files via upload

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kasper/Task/templateofclientservermatching
+# Install script for directory: .
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/kasper/Task/templateofclientservermatching/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "./${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
